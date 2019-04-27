13ce839dac64c968b401c34d21d83d1d9d0ddbbc
hyojong9470 <hyojong9470@gmail.com>
2019-04-27 20:06:13 +0900

    led 릴레이 적용

--- LED.py
+++ LED.py
@@ -2,15 +2,14 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(20, GPIO.OUT)
-
+GPIO.setup(16, GPIO.OUT)
 print("LED STaRT\n")
 
 for i in range(1,11):
     print(i)
-    GPIO.output(20, True)
+    GPIO.output(16, True)
     time.sleep(1)
-    GPIO.output(20, False)
+    GPIO.output(16, False)
     time.sleep(1)
 
 print("END LED")
---
+++ relaytest.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+from time import sleep
+led_pin = 17
+relay_pin = 18
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led_pin, GPIO.OUT)
+GPIO.setup(relay_pin, GPIO.OUT)
+
+
+for i in range(1,3):
+    print("Turn on\n")
+    GPIO.output(relay_pin, False) #False 가 켜짐
+    sleep(2)
+    print("Turn off\n")
+    GPIO.output(relay_pin, True) # True 는 꺼짐
+    sleep(2)
+
+print("종료\n")
+GPIO.cleanup()
